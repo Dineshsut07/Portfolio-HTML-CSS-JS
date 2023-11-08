6c2d15a107f9f86161766e450a88820c8b55705e
Dineshsut07 <120447913+Dineshsut07@users.noreply.github.com>
2023-11-08 17:34:23 +0530

    portfolio

---
+++ script.js
@@ -0,0 +1,9 @@
+var typed = new Typed('.text', {
+    strings: ["Backend Developer", "Fronted Developer", "UI/UX designer"],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 100,
+    loop: true
+});
+
+
